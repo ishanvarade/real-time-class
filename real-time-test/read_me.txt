9db96e559f8dabd748e91ee9d500875a5ba68ea1
root <root@Sydney>
2018-08-05 06:06:49 +0530

    b

---
+++ real-time-test/read_me.txt
@@ -0,0 +1,1 @@
+watch -n 0.1 "dmesg | grep "DequeueOverHead" | tail -n 1 >> abc.txt"
--- real-time-test/src/RTtask_test.cpp
+++ real-time-test/src/RTtask_test.cpp
@@ -9,9 +9,16 @@
 
 #include "RTtask.h"
 #include <iostream>
+#include <climits>
 #include <unistd.h>
 
 #include <stdio.h>
+
+#define TwentyMS 698597200
+#define TenMS 349298600
+#define TwentyMS_loops 32000000
+#define TenMS_loops 15700000
+
 using namespace std;
 
 #define rdtsc_start(cycles_high, cycles_low)			\
@@ -31,37 +38,78 @@ using namespace std;
 typedef unsigned long long ticks;
 static __inline__ ticks getticks(void)
 {
-     unsigned a, d;
-     asm("cpuid");
-     asm volatile("rdtsc" : "=a" (a), "=d" (d));
+	unsigned a, d;
+	asm("cpuid");
+	asm volatile("rdtsc" : "=a" (a), "=d" (d));
 
-     return (((ticks)a) | (((ticks)d) << 32));
+	return (((ticks)a) | (((ticks)d) << 32));
 }
 
 void task_fun(void)
 {
-	unsigned int time = 3000000;
-	usleep(time);
-	cout<<"task_fun\n"<<endl;
+	for (int i = 0; i < TenMS_loops; i++)
+	{
+		asm volatile ( "movl $10, %eax;"
+				"movl $20, %ebx;"
+				"addl %ebx, %eax;"
+		);
+	}
+	cout<<"Printf Ishan :"<<TenMS<<endl;
 }
 
-int main_1()
+int main_1(int argc, char *argv[])
 {
 	struct timespec phase;
-	phase.tv_sec = 4;
+	phase.tv_sec = 15;
 	phase.tv_nsec = 0;
+	__u64 wcet = 100000;
+	__u64 soft_deadline = 1000000;
+	__u64 deadline = 1000000;
+	int core_id = 5;
 
-	RT_task task = RT_task(3000000, 8000000, 9000000, 2, phase, &task_fun);
+	RT_task task = RT_task(wcet, soft_deadline, deadline, core_id, phase, &task_fun);
 	task.start_execution();
 	return 0;
 }
 
 int main(int argc, char *argv[])
 {
-	const int core_id_3 = 3;
+	const int core_id_5 = 5;
 	cpu_set_t cpuset;
 	CPU_ZERO(&cpuset);
-	CPU_SET(core_id_3, &cpuset);
+	CPU_SET(core_id_5, &cpuset);
+
+	sched_setaffinity(getpid(), sizeof(cpu_set_t), &cpuset);
+
+
+	unsigned int time;
+	unsigned int cycles_high = 0, cycles_low = 0;
+	unsigned int cycles_high1 = 0, cycles_low1 = 0;
+	unsigned long long cycle = 0, cycle1 = 0;
+	//ticks tick,tick1,tickh;
+
+	unsigned long long executionTime;
+	//tick = getticks();
+	rdtsc_start(cycles_high, cycles_low);
+
+	task_fun();
+
+	rdtsc_end(cycles_high1, cycles_low1);
+	cycle = cycles_low | ((unsigned long long)cycles_high) <<32;
+	cycle1 = cycles_low1 | ((unsigned long long)cycles_high1) <<32;
+	executionTime = cycle1 - cycle;
+
+	printf ("executionTime: %llu\n", executionTime);
+	return 0;
+}
+
+//698598403
+int main_3(int argc, char *argv[])
+{
+	const int core_id_4 = 4;
+	cpu_set_t cpuset;
+	CPU_ZERO(&cpuset);
+	CPU_SET(core_id_4, &cpuset);
 
 	sched_setaffinity(getpid(), sizeof(cpu_set_t), &cpuset);
 
@@ -73,7 +121,12 @@ int main(int argc, char *argv[])
 	//ticks tick,tick1,tickh;
 
 	//tick = getticks();
-	for (time = 0; time < 10000000; time += 100000)
+	time = 200000;
+	unsigned long long executionTime;
+	unsigned long long cumulativeExecutionTime = 0;
+	unsigned long long averageExecutionTime;
+	unsigned long long minExecutionTime = ULLONG_MAX;
+	for (int i = 0; i < 10000; i++)
 	{
 
 		rdtsc_start(cycles_high, cycles_low);
@@ -94,7 +147,13 @@ int main(int argc, char *argv[])
 		//printf("\ntime in MS%llu\n",tick1-tick);
 		cycle = cycles_low | ((unsigned long long)cycles_high) <<32;
 		cycle1 = cycles_low1 | ((unsigned long long)cycles_high1) <<32;
-		unsigned long long executionTime = cycle1 - cycle;
-		printf ("%llu   %llu\n", time, executionTime);
+		executionTime = cycle1 - cycle;
+		if (executionTime < minExecutionTime)
+			minExecutionTime = executionTime;
+		cumulativeExecutionTime += executionTime;
+		averageExecutionTime = cumulativeExecutionTime / (i + 1);
+		printf ("%llu   %llu	%llu	%llu %llu\n", time, executionTime,
+				cumulativeExecutionTime, averageExecutionTime, minExecutionTime);
 	}
 }
+
